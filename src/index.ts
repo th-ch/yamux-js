a215ed28706f3911f7c5dd6d86e34437affbe13c
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2021-03-19 19:51:41 +0200

    export Stream
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import {Client} from './client';
 import {Config} from './mux';
 import {Server} from './server';
+import {Stream} from './stream';
 
-export {Config, Client, Server};
+export {Config, Client, Server, Stream};
